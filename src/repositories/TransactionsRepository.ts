226750f1eff23fce5ae055104a9dc632bb609d48
Eckart <mhme2000@hotmail.com>
2020-04-16 10:32:10 -0300

    Challenge finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+// import { response } from 'express';
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'outcome' | 'income';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,43 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const findTransactionIncome = this.transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+    const valorInicial = 0;
+    const income: number = findTransactionIncome.reduce(
+      (acumulador: number, valorAtual: Transaction) =>
+        acumulador + valorAtual.value,
+      valorInicial,
+    );
+    const findTransactionOutcome = this.transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+    const valorInicial2 = 0;
+    const outcome: number = findTransactionOutcome.reduce(
+      (acumulador: number, valorAtual: Transaction) =>
+        acumulador + valorAtual.value,
+      valorInicial2,
+    );
+    const total = income - outcome;
+    const balance: Balance = {
+      income,
+      outcome,
+      total,
+    };
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
